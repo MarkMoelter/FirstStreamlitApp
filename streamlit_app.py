139111d1257ba539f7db098d5f6139fa09abf94d
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-12 13:17:03 -0500

    Update streamlit_app.py
    Filter dataframe to only fruits selected

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,9 @@ my_fruit_list = pd.read_csv(text_file)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # allow user selection
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+
                       
 # display the table on the page
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_selected)
