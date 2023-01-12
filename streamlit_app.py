47eac322e97a8df71e7a108c4df27d28d2f12141
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-12 13:15:19 -0500

    Update streamlit_app.py
    Prepopulate multiselector

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,7 +16,7 @@ my_fruit_list = pd.read_csv(text_file)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # allow user selection
-streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index))
+streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
                       
 # display the table on the page
 streamlit.dataframe(my_fruit_list)
