005c60e4bd10c45186a73fd174c26a9ca19d5150
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-12 13:18:49 -0500

    Update streamlit_app.py
    Change dataframe variable to show

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,4 +21,4 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 
                       
 # display the table on the page
-streamlit.dataframe(fruits_selected)
+streamlit.dataframe(fruits_to_show)
