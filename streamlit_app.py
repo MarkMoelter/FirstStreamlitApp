d0ac3f7b0117503a12368001db33ea7efd0f1e7d
MarkMoelter <108141366+MarkMoelter@users.noreply.github.com>
2023-01-12 21:05:29 -0500

    Update streamlit_app.py
    Import requests

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,10 +15,11 @@ text_file = 'https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.
 my_fruit_list = pd.read_csv(text_file)
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-# allow user selection
 fruits_selected = streamlit.multiselect('Pick some fruits:', list(my_fruit_list.index), ['Avocado', 'Strawberries'])
 fruits_to_show = my_fruit_list.loc[fruits_selected]
-
                       
-# display the table on the page
 streamlit.dataframe(fruits_to_show)
+
+import requests
+fruityvice_response = requests.get('https://fruityvice.com/api/fruit/watermelon')
+streamlit.text(fruityvice_response)
